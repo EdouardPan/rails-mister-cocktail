06512528b037420fefb4dc9a96e06ceab120e618
Edouard Panossian <edouard.panossian@gmail.com>
2018-05-18 13:55:31 +0200

    prepare for Heroku

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,7 +1,40 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [ :show, :destroy ]
 
   def index
     @cocktails = Cocktail.all
+  end
+
+  def show
+    @new_dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :url_img)
   end
 
 
